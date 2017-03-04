129ee9d5d2dd802b410adf4c263a417f14d1e837
adutt2 <adutt2@hawk.iit.edu>
2017-03-03 19:24:45 -0600

    Update MaxTemperatureWithCompression.java

--- MaxTemperatureWithCompression.java
+++ MaxTemperatureWithCompression.java
@@ -1,21 +1,41 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class MaxTemperatureWithCompression {
-public static void main(String[] args) throws Exception {
-if (args.length != 2) {
-System.err.println("Usage: MaxTemperatureWithCompression <input path> " +
-"<output path>");
-System.exit(-1);
-}
-Job job = new Job();
-job.setJarByClass(MaxTemperature.class);
-FileInputFormat.addInputPath(job, new Path(args[0]));
-FileOutputFormat.setOutputPath(job, new Path(args[1]));
-job.setOutputKeyClass(Text.class);
-job.setOutputValueClass(IntWritable.class);
-FileOutputFormat.setCompressOutput(job, true);
-FileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);
-job.setMapperClass(MaxTemperatureMapper.class);
-job.setCombinerClass(MaxTemperatureReducer.class);
-job.setReducerClass(MaxTemperatureReducer.class);
-System.exit(job.waitForCompletion(true) ? 0 : 1);
-}
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.println("Usage: MaxTemperatureWithMapOutputCompression " +
+        "<input path> <output path>");
+      System.exit(-1);
+    }
+
+    // vv MaxTemperatureWithMapOutputCompression
+    Configuration conf = new Configuration();
+    conf.setBoolean(Job.MAP_OUTPUT_COMPRESS, true);
+    conf.setClass(Job.MAP_OUTPUT_COMPRESS_CODEC, GzipCodec.class,
+        CompressionCodec.class);
+    Job job = new Job(conf);
+    // ^^ MaxTemperatureWithMapOutputCompression
+    job.setJarByClass(MaxTemperature.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.setMapperClass(MaxTemperatureMapper.class);
+    job.setCombinerClass(MaxTemperatureReducer.class);
+    job.setReducerClass(MaxTemperatureReducer.class);
+    
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
