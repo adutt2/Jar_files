575201e195c0c2ec9143e03286b67472d73d54db
adutt2 <adutt2@hawk.iit.edu>
2017-03-16 15:49:06 -0500

    Add files via upload

---
+++ week-08/Item2/MaxTemperatureReducer.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MaxTemperatureReducer
+  extends Reducer<Text, IntWritable, Text, Text> {
+	String temp = null;
+  
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values,
+      Context context)
+      throws IOException, InterruptedException {
+    
+    int maxValue = Integer.MIN_VALUE;
+    int minValue = Integer.MAX_VALUE;
+    for (IntWritable value : values) {
+      maxValue = Math.max(maxValue, value.get());
+      minValue = Math.min(minValue, value.get());
+      String max, min;
+      max = Integer.toString(maxValue);
+      min = Integer.toString(minValue);
+      temp = max + " " + min;
+      
+    }
+    context.write(key, new Text(temp));
+  }
+}
