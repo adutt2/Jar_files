4083d80b6e67f3029bcf9375da89dd0d11b67a5f
adutt2 <adutt2@hawk.iit.edu>
2017-03-16 12:35:24 -0500

    Update MaxTemperatureMapper2.java

--- Item6/MaxTemperatureMapper2.java
+++ Item6/MaxTemperatureMapper2.java
@@ -1,35 +1,21 @@
-package oldapi;
-
 import java.io.IOException;
-
+import java.lang.Iterable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class MaxTemperatureMapper2 extends MapReduceBase
-  implements Mapper<LongWritable, Text, Text, IntWritable> {
+public class MaxTemperatureMapper2
+  extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-  private static final int MISSING = 9999;
-  
-  public void map(LongWritable key, Text value,
-      OutputCollector<Text, IntWritable> output, Reporter reporter)
-      throws IOException {
-    
-    String line = value.toString();
-    String year = line.substring(15, 19);
-    int airTemperature;
-    if (line.charAt(87) == '+') { // parseInt doesn't like leading plus signs
-      airTemperature = Integer.parseInt(line.substring(88, 92));
-    } else {
-      airTemperature = Integer.parseInt(line.substring(87, 92));
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+       int maxValue = Integer.MIN_VALUE;
+    for (IntWritable value : values) {
+      maxValue = Math.max(maxValue, value.get());
     }
-    String quality = line.substring(92, 93);
-    if (airTemperature != MISSING && quality.matches("[01459]")) {
-      output.collect(new Text(year), new IntWritable(airTemperature));
-    }
+    context.write(key, new IntWritable(maxValue));
+
   }
 }
