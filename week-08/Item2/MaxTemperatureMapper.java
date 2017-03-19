aa5ed9ae2051bece80d7d55697f1b4cfc4357a28
adutt2 <adutt2@hawk.iit.edu>
2017-03-18 19:33:53 -0500

    Update MaxTemperatureMapper.java

--- week-08/Item2/MaxTemperatureMapper.java
+++ week-08/Item2/MaxTemperatureMapper.java
@@ -1,7 +1,4 @@
-// cc MaxTemperatureMapper Mapper for maximum temperature example
-// vv MaxTemperatureMapper
 import java.io.IOException;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -19,15 +16,19 @@ public class MaxTemperatureMapper
     String line = value.toString();
     String year = line.substring(15, 19);
     int airTemperature;
+try{
     if (line.charAt(87) == '+') { // parseInt doesn't like leading plus signs
       airTemperature = Integer.parseInt(line.substring(88, 92));
-    } else {
+    }else{
       airTemperature = Integer.parseInt(line.substring(87, 92));
     }
+}
+catch (NumberFormatException e){
+airTemperature = 00000;
+}
     String quality = line.substring(92, 93);
     if (airTemperature != MISSING && quality.matches("[01459]")) {
       context.write(new Text(year), new IntWritable(airTemperature));
     }
   }
 }
-// ^^ MaxTemperatureMapper
